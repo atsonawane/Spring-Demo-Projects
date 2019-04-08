a04459b3121df998922cffbcf22f56c4b670c66b
Avinash <avinashsonawane55@gmail.com>
2019-04-08 10:22:01 +0530

    Add files via upload

---
+++ IOC_SI_Map/src/Test/Test.java
@@ -0,0 +1,20 @@
+package Test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import beans.Question;
+
+public class Test {
+	public static void main(String[] args) {
+		Resource resource = new ClassPathResource("resources/applicationContext.xml");
+		BeanFactory beanFactory = new XmlBeanFactory(resource);
+		
+		Question question = (Question) beanFactory.getBean("q");
+		
+		question.displayInfo();
+		
+	}
+}
